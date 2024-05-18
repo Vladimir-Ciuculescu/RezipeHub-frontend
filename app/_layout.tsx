cfefe886ece42acde031cb8b161258d9d8ccfe6d
Vladi <vladimir.ciuculescu@gmail.com>
2024-05-18 14:06:21 +0300

    Configured fonts and prettier

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,76 +1,44 @@
-import { Text, View, ViewStyle, Dimensions } from "react-native";
-import Logo from "../assets/svg/Logo.svg";
+import { Text, View, ViewStyle, Dimensions, StyleSheet } from 'react-native';
 
-const { width, height } = Dimensions.get("screen");
+const { width, height } = Dimensions.get('screen');
 
-import {
-  useFonts,
-  SofiaSans_100Thin,
-  SofiaSans_200ExtraLight,
-  SofiaSans_300Light,
-  SofiaSans_400Regular,
-  SofiaSans_500Medium,
-  SofiaSans_600SemiBold,
-  SofiaSans_700Bold,
-  SofiaSans_800ExtraBold,
-  SofiaSans_900Black,
-  SofiaSans_100Thin_Italic,
-  SofiaSans_200ExtraLight_Italic,
-  SofiaSans_300Light_Italic,
-  SofiaSans_400Regular_Italic,
-  SofiaSans_500Medium_Italic,
-  SofiaSans_600SemiBold_Italic,
-  SofiaSans_700Bold_Italic,
-  SofiaSans_800ExtraBold_Italic,
-  SofiaSans_900Black_Italic,
-} from "@expo-google-fonts/sofia-sans";
-import { colors } from "@/theme/colors";
+import { colors } from '@/theme/colors';
+import { useFonts } from '@expo-google-fonts/sofia-sans';
+import { fonts, fontsToLoad } from '@/theme/typography';
 
 export default function RootLayout() {
-  let [fontsLoaded] = useFonts({
-    SofiaSans_100Thin,
-    SofiaSans_200ExtraLight,
-    SofiaSans_300Light,
-    SofiaSans_400Regular,
-    SofiaSans_500Medium,
-    SofiaSans_600SemiBold,
-    SofiaSans_700Bold,
-    SofiaSans_800ExtraBold,
-    SofiaSans_900Black,
-    SofiaSans_100Thin_Italic,
-    SofiaSans_200ExtraLight_Italic,
-    SofiaSans_300Light_Italic,
-    SofiaSans_400Regular_Italic,
-    SofiaSans_500Medium_Italic,
-    SofiaSans_600SemiBold_Italic,
-    SofiaSans_700Bold_Italic,
-    SofiaSans_800ExtraBold_Italic,
-    SofiaSans_900Black_Italic,
-  });
+	let [fontsLoaded] = useFonts(fontsToLoad);
 
-  if (!fontsLoaded) {
-    return null;
-  }
+	if (!fontsLoaded) {
+		return null;
+	}
 
-  return (
-    <View style={$containerStyle}>
-      <View style={$baseContainerStyle}></View>
-    </View>
-  );
+	return (
+		<View style={$containerStyle}>
+			<View style={$baseContainerStyle}>
+				<Text
+					selectable
+					disabled
+					style={{ fontFamily: fonts.sofia100, fontSize: 30 }}>
+					adw
+				</Text>
+			</View>
+		</View>
+	);
 }
 
 const $containerStyle: ViewStyle = {
-  flex: 1,
-  backgroundColor: "#70B9BE",
-  justifyContent: "flex-end",
-  alignItems: "center",
+	flex: 1,
+	backgroundColor: '#70B9BE',
+	justifyContent: 'flex-end',
+	alignItems: 'center',
 };
 
 const $baseContainerStyle: ViewStyle = {
-  flexShrink: 0,
-  height: height / 3.5,
-  width: width,
-  backgroundColor: colors.greyscale100,
-  borderTopLeftRadius: 16,
-  borderTopRightRadius: 16,
+	flexShrink: 0,
+	height: height / 3.5,
+	width: width,
+	backgroundColor: colors.greyscale100,
+	borderTopLeftRadius: 16,
+	borderTopRightRadius: 16,
 };
