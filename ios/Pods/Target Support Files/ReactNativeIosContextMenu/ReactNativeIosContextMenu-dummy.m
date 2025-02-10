c43573e58c980e4be79edc4246aaa3c2917559e1
Vladi <vladimir.ciuculescu@gmail.com>
2025-02-10 14:49:15 +0200

    removed native folders from track

--- ios/Pods/Target Support Files/ReactNativeIosContextMenu/ReactNativeIosContextMenu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ReactNativeIosContextMenu : NSObject
-@end
-@implementation PodsDummy_ReactNativeIosContextMenu
-@end
--- ios/Pods/Target Support Files/ReactNativeIosUtilities/ReactNativeIosUtilities-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ReactNativeIosUtilities : NSObject
-@end
-@implementation PodsDummy_ReactNativeIosUtilities
-@end
